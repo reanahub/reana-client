56f136499e84f6b54f33e6ba1ad2b3563bcaac40
Tibor Simko <tibor.simko@cern.ch>
2018-11-20 11:25:35 +0100

    installation: fix pytest-reana dependency version
    Signed-off-by: Tibor Simko <tibor.simko@cern.ch>

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ tests_require = [
     'pytest-cache>=1.0',
     'pytest-cov>=1.8.0',
     'pytest-pep8>=1.0.6',
-    'pytest-reana>=v0.5.0.dev20181119'
+    'pytest-reana>=0.5.0.dev20181119'
 ]
 
 extras_require = {

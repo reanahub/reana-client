02fda3be1e4fe9c73aef2309888088955356b336
Tibor Šimko <tibor.simko@cern.ch>
2020-05-25 11:19:53 +0200

    installation: six and webcolors versions
    Signed-off-by: Tibor Šimko <tibor.simko@cern.ch>

--- setup.py
+++ setup.py
@@ -51,10 +51,10 @@ install_requires = [
     'jsonpointer>=2.0',
     'reana-commons>=0.6.0,<0.7.0',
     'rfc3987>=1.3.8',  # FIXME remove once yadage-schemas solves deps.
-    'six==1.12.0',
+    'six>=1.12.0,<1.16.0',
     'strict-rfc3339>=0.7',  # FIXME remove once yadage-schemas solves deps.
     'tablib>=0.12.1,<0.13',
-    'webcolors==1.9.1',  # FIXME remove once yadage-schemas solves deps.
+    'webcolors>=1.9.1,<1.12.0',  # FIXME remove once yadage-schemas solves deps.
     'werkzeug>=0.14.1',
     'yadage-schemas==0.10.6',
 ]

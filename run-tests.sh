54b5aa0244cdc98dac811355fbfb8802935af90c
Marco Vidal García <marco.vidal@cern.ch>
2020-07-23 14:32:24 +0200

    Revert "docs: add script to generate cli api docs"
    This reverts commit 44811f7f8b3f4af5711fb3813182d4d9638437fb.

--- run-tests.sh
+++ run-tests.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 #
 # This file is part of REANA.
 # Copyright (C) 2017, 2018 CERN.
@@ -6,22 +6,11 @@
 # REANA is free software; you can redistribute it and/or modify it
 # under the terms of the MIT License; see LICENSE file for more details.
 
-
-cli_docs_url=https://raw.githubusercontent.com/reanahub/docs.reana.io/master/docs/reference/reana-client-cli-api/index.md
-docs_differ_error_msg='Current reana-client differs with the documentation. Please update http://docs.reana.io/reference/reana-client-cli-api/.'
-python_version=$(python -c 'import sys;  print(sys.version_info.major)')
-
 pydocstyle reana_client && \
 black --check . && \
 reana-client --help > cmd_list.txt && \
 diff -q -w docs/cmd_list.txt cmd_list.txt  && \
 rm cmd_list.txt && \
-if [ "$python_version" -eq 3 ]
-then
-    python scripts/generate_cli_api.py > cli_api.md && \
-    (diff -q -w  cli_api.md <(curl -s $cli_docs_url) || (echo $docs_differ_error_msg && exit 1)) && \
-    rm cli_api.md
-fi
 check-manifest --ignore ".travis-*" && \
 sphinx-build -qnNW docs docs/_build/html && \
 python setup.py test && \

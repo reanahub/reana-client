fc337db6542e3b04af31423bca147029d8d5844b
Marco Vidal García <marco.vidal@cern.ch>
2021-11-22 11:42:03 +0100

    installation: bump shared modules

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ readme = open("README.rst").read()
 history = open("CHANGES.rst").read()
 
 tests_require = [
-    "pytest-reana>=0.8.0a7,<0.9.0",
+    "pytest-reana>=0.8.0,<0.9.0",
 ]
 
 extras_require = {
@@ -41,7 +41,7 @@ install_requires = [
     "click>=7",
     "cwltool==3.1.20210628163208",
     "jsonpointer>=2.0",
-    "reana-commons[yadage,snakemake]>=0.8.0a37,<0.9.0",
+    "reana-commons[yadage,snakemake]>=0.8.0,<0.9.0",
     "tablib>=0.12.1,<0.13",
     "werkzeug>=0.14.1",
 ]

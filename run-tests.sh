9d90a586b3c4be59fe58ca6fa7b9ef3658eecd29
Tibor Šimko <tibor.simko@cern.ch>
2026-03-11 09:34:26 +0100

    ci(run-tests): add usage help and refactor options (#762)

--- run-tests.sh
+++ run-tests.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 #
 # This file is part of REANA.
-# Copyright (C) 2017, 2018, 2019, 2020, 2022, 2024 CERN.
+# Copyright (C) 2017, 2018, 2019, 2020, 2022, 2024, 2025 CERN.
 #
 # REANA is free software; you can redistribute it and/or modify it
 # under the terms of the MIT License; see LICENSE file for more details.
@@ -9,11 +9,46 @@
 set -o errexit
 set -o nounset
 
-check_commitlint () {
+docker_build() {
+    docker build -t docker.io/reanahub/reana-client .
+}
+
+docker_run() {
+    docker run --rm -v "$PWD"/tests:/home/reana/tests --entrypoint /bin/bash docker.io/reanahub/reana-client -c 'pytest tests'
+}
+
+docs_cli_api() {
+    cli_docs_url=https://raw.githubusercontent.com/reanahub/docs.reana.io/master/docs/reference/reana-client-cli-api/index.md
+    docs_differ_error_msg='Current reana-client differs with the documentation. Please update http://docs.reana.io/reference/reana-client-cli-api/.'
+    python scripts/generate_cli_api.py > cli_api.md
+    (diff -q -w  cli_api.md <(curl -s $cli_docs_url) || (echo "$docs_differ_error_msg" && exit 1))
+    rm cli_api.md
+}
+
+docs_cli_commands() {
+    reana-client --help > cmd_list.txt
+    diff -q -w docs/cmd_list.txt cmd_list.txt
+    rm cmd_list.txt
+}
+
+docs_sphinx() {
+    sphinx-build -qnNW docs docs/_build/html
+    sphinx-build -qnNW -b doctest docs docs/_build/doctest
+}
+
+format_black() {
+    black --check .
+}
+
+lint_commitlint() {
     from=${2:-master}
     to=${3:-HEAD}
     pr=${4:-[0-9]+}
-    npx commitlint --from="$from" --to="$to"
+    if command -v commitlint >/dev/null 2>&1; then
+        commitlint --from="$from" --to="$to"
+    else
+        npx commitlint --from="$from" --to="$to"
+    fi
     found=0
     while IFS= read -r line; do
         commit_hash=$(echo "$line" | cut -d ' ' -f 1)
@@ -31,7 +66,7 @@ check_commitlint () {
         # (iii) check absence of merge commits in feature branches
         if [ "$commit_number_of_parents" -gt 1 ]; then
             if echo "$commit_title" | grep -qE "^chore\(.*\): merge "; then
-                break  # skip checking maint-to-master merge commits
+                break # skip checking maint-to-master merge commits
             else
                 echo "✖   Merge commits are not allowed in feature branches: $commit_title"
                 found=1
@@ -43,96 +78,87 @@ check_commitlint () {
     fi
 }
 
-check_shellcheck () {
-    find . -name "*.sh" -exec shellcheck {} \+
-}
-
-check_black () {
-    black --check .
-}
-
-check_flake8 () {
+lint_flake8() {
     flake8 .
 }
 
-check_pydocstyle () {
-    pydocstyle reana_client
-}
-
-check_manifest () {
-    check-manifest
-}
-
-check_cli_cmds () {
-    reana-client --help > cmd_list.txt
-    diff -q -w docs/cmd_list.txt cmd_list.txt
-    rm cmd_list.txt
-}
-
-check_cli_api () {
-    cli_docs_url=https://raw.githubusercontent.com/reanahub/docs.reana.io/master/docs/reference/reana-client-cli-api/index.md
-    docs_differ_error_msg='Current reana-client differs with the documentation. Please update http://docs.reana.io/reference/reana-client-cli-api/.'
-    python scripts/generate_cli_api.py > cli_api.md
-    (diff -q -w  cli_api.md <(curl -s $cli_docs_url) || (echo "$docs_differ_error_msg" && exit 1))
-    rm cli_api.md
-}
-
-check_dockerfile () {
+lint_hadolint() {
     docker run -i --rm docker.io/hadolint/hadolint:v2.12.0 < Dockerfile
 }
 
-check_docker_build () {
-    docker build -t docker.io/reanahub/reana-client .
+lint_manifest() {
+    check-manifest
 }
 
-check_docker_run () {
-    docker run --rm -v "$PWD"/tests:/home/reana/tests --entrypoint /bin/bash docker.io/reanahub/reana-client -c 'pytest tests'
+lint_pydocstyle() {
+    pydocstyle reana_client
 }
 
-check_sphinx () {
-    sphinx-build -qnNW docs docs/_build/html
-    sphinx-build -qnNW -b doctest docs docs/_build/doctest
+lint_shellcheck() {
+    find . -name "*.sh" -exec shellcheck {} \+
 }
 
-check_pytest () {
+python_tests() {
     pytest
 }
 
-check_all() {
-    check_commitlint
-    check_shellcheck
-    check_black
-    check_flake8
-    check_pydocstyle
-    check_manifest
-    check_cli_cmds
-    check_cli_api
-    check_dockerfile
-    check_docker_build
-    check_docker_run
-    check_sphinx
-    check_pytest
+all() {
+    docker_build
+    docker_run
+    docs_cli_api
+    docs_cli_commands
+    docs_sphinx
+    format_black
+    lint_commitlint
+    lint_flake8
+    lint_hadolint
+    lint_manifest
+    lint_pydocstyle
+    lint_shellcheck
+    python_tests
+}
+
+help() {
+    echo "Usage: $0 [options]"
+    echo "Options:"
+    echo "  --all                  Perform all checks [default]"
+    echo "  --docker-build         Check Docker build"
+    echo "  --docker-run           Check Docker run"
+    echo "  --docs-cli-api         Check CLI API docs"
+    echo "  --docs-cli-commands    Check CLI commands list"
+    echo "  --docs-sphinx          Check Sphinx docs build"
+    echo "  --format-black         Check formatting of Python code"
+    echo "  --help                 Display this help message"
+    echo "  --lint-commitlint      Check linting of commit messages"
+    echo "  --lint-flake8          Check linting of Python code"
+    echo "  --lint-hadolint        Check linting of Dockerfiles"
+    echo "  --lint-manifest        Check linting of Python manifest"
+    echo "  --lint-pydocstyle      Check linting of Python docstrings"
+    echo "  --lint-shellcheck      Check linting of shell scripts"
+    echo "  --python-tests         Check Python test suite"
 }
 
 if [ $# -eq 0 ]; then
-    check_all
+    all
     exit 0
 fi
 
 arg="$1"
 case $arg in
-    --check-commitlint) check_commitlint "$@";;
-    --check-shellcheck) check_shellcheck;;
-    --check-black) check_black;;
-    --check-flake8) check_flake8;;
-    --check-pydocstyle) check_pydocstyle;;
-    --check-manifest) check_manifest;;
-    --check-cli-cmds) check_cli_cmds;;
-    --check-cli-api) check_cli_api;;
-    --check-dockerfile) check_dockerfile;;
-    --check-docker-build) check_docker_build;;
-    --check-docker-run) check_docker_run;;
-    --check-sphinx) check_sphinx;;
-    --check-pytest) check_pytest;;
-    *) echo "[ERROR] Invalid argument '$arg'. Exiting." && exit 1;;
+--all) all ;;
+--help) help ;;
+--docker-build) docker_build ;;
+--docker-run) docker_run ;;
+--docs-cli-api) docs_cli_api ;;
+--docs-cli-commands) docs_cli_commands ;;
+--docs-sphinx) docs_sphinx ;;
+--format-black) format_black ;;
+--lint-commitlint) lint_commitlint "$@" ;;
+--lint-flake8) lint_flake8 ;;
+--lint-hadolint) lint_hadolint ;;
+--lint-manifest) lint_manifest ;;
+--lint-pydocstyle) lint_pydocstyle ;;
+--lint-shellcheck) lint_shellcheck ;;
+--python-tests) python_tests ;;
+*) echo "[ERROR] Invalid argument '$arg'. Exiting." && help && exit 1 ;;
 esac
